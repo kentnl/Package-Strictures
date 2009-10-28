86f511092adeec118c1b39ce39cf927d9c4f0f0c
Kent Fredric <kentfredric@gmail.com>
2009-10-28 19:31:47 +1300

    fix spelling

--- lib/Package/Strictures/Register.pm
+++ lib/Package/Strictures/Register.pm
@@ -3,7 +3,7 @@ use warnings;
 
 package Package::Strictures::Register;
 
-# ABSTRACT: Create compile-time constants that can be tweaked by userland with Package::Strictures.
+# ABSTRACT: Create compile-time constants that can be tweaked by users with Package::Strictures.
 
 1;
 
