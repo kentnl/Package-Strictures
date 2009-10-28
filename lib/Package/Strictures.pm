d3a6f9aeb51604e8ceb18232a2515ac64070dac2
Kent Fredric <kentfredric@gmail.com>
2009-10-28 22:39:54 +1300

    documented, refactored nicely

--- lib/Package/Strictures.pm
+++ lib/Package/Strictures.pm
@@ -5,5 +5,54 @@ package Package::Strictures;
 
 # ABSTRACT: Facilitate toggling validation code at users request, without extra performance penalties.
 
+
+=head1 DESCRIPTION
+
+Often, I find myself in a bind, where I have code I want to do things properly, so it will detect
+of its own accord ( at runtime ) misuses of varying data-structures or methods, but the very same
+tools that would be used to analysse and assure that things are going correctly, result in substantial
+performance penalties.
+
+This module, and the infrastructure I hope builds on top of it, may hopefully provide an 'in' that lets me have the best of both worlds,
+fast on the production server, and concise when trying to debug it ( that is, not having to manually desk-check the whole execution cycle
+through various functions and modules just to find which level things are going wrong at ).
+
+In an ideal world, code would be both fast and concise, however, that is a future fantasy, and this here instead aims to produce 80% of the same
+beneifts, but now, instead of never.
+
+=head1 SYNOPSIS
+
+=head2 IMPLEMENTING MODULES
+
+  package Foo::Bar::Baz;
+
+  use Package::Strictures::Register -setup => {
+      -strictures => {
+          STRICT => {
+            default => ''¸
+          },
+      },
+  };
+
+  if( STRICT ) {
+    /* Elimintated Code */
+  }
+
+=head2 CONSUMING USERS
+
+  use Package::Strictures -for => {
+    'Foo::Bar::Baz' => {
+      'STRICT' => 1,
+    },
+  };
+
+  use Foo::Bar::Baz;
+
+  /* Previously eliminated code now runs.
+
+
+=cut
+
+
 1;
 
--- lib/Package/Strictures/Register.pm
+++ lib/Package/Strictures/Register.pm
@@ -15,42 +15,83 @@ sub import {
     Carp::carp( __PACKAGE__ . " called with no parameters, skipping magic" );
     return;
   }
+
   my (@caller) = caller();
   my $package = $caller[0];
   $package = $params{-for} if exists $params{-for};
-
-  my %config;
 
   if ( not exists $params{-setup} ) {
     Carp::croak("Can't setup strictures for package '$package', need -setup ");
   }
 
-  %config = %{$params{-setup}};
+  $self->_setup( $params{-setup}, $package );
 
-  $config{-strictures} = {} unless exists $config{-strictures};
+}
 
-  my $reftype = ref $config{-strictures};
+sub _setup {
+  my ( $self, $params, $package ) = @_;
+
+  my $reftype = ref $params;
+
+  if ( not $reftype eq 'HASH' ) {
+    Carp::croak(qq/ -setup => can presently only support a HASH. Got '$reftype'/);
+  }
+
+  if ( ( not exists $params->{-strictures} ) && ( not exists $params->{-groups} ) ) {
+    Carp::croak('Neither -setup => { -strictures }  or -setup => { -groups } provided.');
+  }
+
+  if ( exists $params->{-groups} ) {
+    Carp::carp('-groups support is not yet implemented');
+  }
+
+  $params->{-strictures} = {} unless exists $params->{-strictures};
+  $params->{-groups}     = {} unless exists $params->{-groups};
+
+  $self->_setup_strictures( $params->{-strictures}, $package );
+
+}
+
+sub _setup_strictures {
+  my ( $self, $strictures, $package ) = @_;
+  my $reftype = ref $strictures;
 
   if ( not $reftype eq 'HASH' ) {
     Carp::croak( qq/Can't handle anything except a HASH ( Got $reftype )/
         . qq/ for param -setup => { -strictures =>  } in -setup for $package/ );
   }
-  my $subs = $self->create_coderefs( $config{-strictures}, $package );
 
-}
-
-sub create_coderefs {
-  my ( $self, $strictures, $target ) = @_;
   for my $subname ( keys %{$strictures} ) {
-    my $code = sub() { 0 };
-    {
-      no strict 'refs';
-      *{$target . '::' . $subname } = $code;
-    }
 
+    $self->_setup_stricture( $strictures->{$subname}, $package, $subname );
   }
 }
 
+sub _setup_stricture {
+  my ( $self, $prototype, $package, $name ) = @_;
+  if ( not exists $prototype->{default} ) {
+    Carp::croak("Prototype for `$package`::`$name` lacks a [required] ->{'default'} ");
+  }
+
+  $self->_advertise_stricture( $package, $name );
+  my $value = $self->_fetch_stricture_value( $package, $name, $prototype->{default} );
+  my $code = sub() { $value };
+  {
+    no strict 'refs';
+    *{ $package . '::' . $name } = $code;
+  }
+}
+
+sub _advertise_stricture {
+  my ( $self, $package, $name ) = @_;
+
+}
+
+sub _fetch_stricture_value {
+  my ( $self, $package, $name, $default ) = @_;
+
+  return $default;
+}
 
 1;
 
